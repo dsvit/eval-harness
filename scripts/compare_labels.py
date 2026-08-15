21dbc716aabb65afa999fd6b53fb3b13c83cb5d4
Vittorio Oddo <vittorio.oddo@outlook.de>
2026-08-15 08:00:19 +0200

    Fälle als Dict mit id als Schlüssel

--- scripts/compare_labels.py
+++ scripts/compare_labels.py
@@ -1,5 +1,20 @@
-text = open("data/blind.jsonl", encoding="utf-8").read() # Durch open öffnen wir den Datei-Inhalt. Mit .read() lesen wir den gesamten Inhalt von blind.jsonl und geben ihn zurück.
+import json
 
-lines = text.splitlines() # Zerlegt den Text an den Umbrüchen
+# --- Datei einlesen ---
 
-print(len(lines)) # Gibt uns die Anzahl der Zeilen aus. Da eine Zeile einen Fall abbildet, kann man auch sagen, dass uns das die Anzahl an Fälle zurückgibt.
+# Durch open öffnen wir den Datei-Inhalt. Mit .read() lesen wir den gesamten Inhalt von blind.jsonl und geben ihn zurück.
+
+text = open("data/blind.jsonl", encoding="utf-8").read()
+
+# Zerlegt den Text an den Umbrüchen
+lines = text.splitlines()
+
+
+# --- Eine Zeile in ein Dict umwandeln ---
+
+blind = {} # Das bildet das Dict ab in dem die einzelnen Fälle gespeichert werden.
+
+# Jede Zeile in ein Dict umwandeln und unter ihrer id ablegen,damit beide Dateien später über die id verglichen werden können.
+for zeile in lines:
+    fall = json.loads(zeile)
+    blind[fall["id"]] = fall
